78d2a4f23eb6201256bbb74cb2c46e4c318a00a8
priya8 <panditpriyanka93@gmail.com>
2014-05-08 22:55:40 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -7,10 +7,7 @@ $lod=time();
 $message=$subject=$to="";
 $systemdate=date("Y");
 $con=mysql_connect("localhost","root","");
-
-
 mysql_query("CREATE DATABASE cyb_sign_in",$con);
-
 mysql_select_db("cyb_sign_in");
 $sql= "CREATE TABLE sign_in ( 
            name varchar(20),
@@ -22,12 +19,7 @@ $sql= "CREATE TABLE sign_in (
            institution varchar(50),
            rno int(10),
            active int(3),
-           ld bigint(30)
-           
-           
-           )" ; 
-
-
+           ld bigint(30))" ; 
 if($_POST)
 {  
 $name=$_POST["t1"];
@@ -38,18 +30,13 @@ $edu=$_POST["t5"];
 $insti=$_POST["t6"];
 $pass1=$_POST["t7"];
 $pass2=$_POST["t8"];
-
-
 mysql_query($sql,$con);
 require_once "Mail.php";
-
 $from = '<x@gmail.com>';
 $to = $mail;
 $subject = 'Hi!';
 $link='http://localhost/activate.php';
-
 $body = "Hi,\n\nThanks for signing up to CYB!!! Discover yourself!$link  Activate your account within a period of 5 days. If not, you will have to sign-up again.";
-
 $headers = array(
    'From' => $from,
    'To' => $to,
@@ -58,7 +45,6 @@ $headers = array(
    'MIME-Version'=>'1.0',    
 );
 
-
 $smtp = Mail::factory('smtp', array(
        'host' => 'ssl://smtp.gmail.com',
        'port' => '465',
@@ -66,10 +52,8 @@ $smtp = Mail::factory('smtp', array(
        'username' => 'x@gmail.com',
        'password' => 'password_of_x@gmail.com'
    ));
-
  if(!empty($name))
      {
-
 if (!preg_match( "/^[a-zA-Z ]*$/",$name))
          {
              $error_name="invalid name field";
@@ -97,17 +81,14 @@ else
 
                    if ($age<13){ 
                                 $message = "Hello young friend, We are extremely sorry for your inconvenience 
-
 in creating the account at CYB. Please read the Terms and Conditions to know more";
                    echo "<script type='text/javascript'>alert('$message');</script>";
                                }
                      else {
                             $v2=1;
                             }
-                   
-       }
-
-  else 
+                    }
+ else 
 { $error_dob="Please Enter valid dob";}
 
 
@@ -120,26 +101,19 @@ if(!empty($mail))
     else 
      {
      $v3=1;
-     
-    
-    $query=mysql_query("select count(mail) from sign_in where mail='$mail'");
+     $query=mysql_query("select count(mail) from sign_in where mail='$mail'");
     $query20=mysql_query("select active from sign_in where mail='$mail'");
 $n20=mysql_result($query20,0);
     if(mysql_result($query,0)>=1 && $n20==1)  
    {$error_mail="already exists";
 $v3=0;}
 else if(mysql_result($query,0)>=1 && $n20==0){mysql_query("DELETE FROM sign_in WHERE mail='".$mail."'"); $v3=1;} 
-
-
- 
 }
 }
 else  
 {
        $error_mail="Empty Field!!!";
 }
-
-
 if($pass1==$pass2)
 {   
 if(!preg_match('/^[0-9A-Za-z!@#$%]{6,15}$/', $pass1))
@@ -149,8 +123,6 @@ else {$v4=1;}
 }
 else
 $error_pwd2="Password didn't match";
-
-
 if(!empty($mobile))
 {
     if(!preg_match("/^\d{10}$/",$mobile))
@@ -181,45 +153,29 @@ else
 {
     $error_insti="Empty Field!!!";
 }
-
-
 if($age<13 && !empty($dob)){$v1=$v2=$v3=$v4=$v5=$v6=$v7=0;
 $error_name=$error_dob=$error_mail=$error_mobile=$error_edu=$error_insti=$error_pwd1=$error_pwd2="";
 
 }
-                    
-$valid=$v1*$v2*$v3*$v4*$v5*$v6*$v7;
+                    $valid=$v1*$v2*$v3*$v4*$v5*$v6*$v7;
     
 if($valid)
 {
 
 setcookie('m',$mail,time()+120);
-
 $num=rand(1,23);
-
 $sql1="INSERT INTO sign_in(name,dob,mail,mobile,password,education,institution,rno,active,ld)VALUES
-
 ('$name','$dob','$mail','$mobile','$pass1','$edu','$insti','$num','$a','$lod')";
-
 mysql_query($sql1,$con);
-
 mysql_close($con);
 header( 'Location: thankyou.html' ) ; 
-
 $mail = $smtp->send($to, $headers, $body);
-
 if (PEAR::isError($mail)) {
-   echo('<p>' . $mail->getMessage() . '</p>');
+           echo('<p>' . $mail->getMessage() . '</p>');
 } else {
    echo('<p>Message successfully sent!</p>');
 }}
-
-
 }
-
-
 ?>
-
-
 
 
